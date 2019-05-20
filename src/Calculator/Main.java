6ee5e9022a42e545c889aa667ff804205c9d8adb
RSingh39 <rishabh_singh39@yahoo.com>
2019-05-20 14:43:15 -0700

    Java OOP Calculator

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,101 +1,52 @@
 package Calculator;
 
 import java.util.Scanner;
-import java.text.NumberFormat;
 
-public class Main
-{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+class Calculator{
+    int a, b;
+    Calculator(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+    // addition
+    int add() { return a + b;}
+    // subtraction
+    int subtract() { return a - b;}
+    // multiplication
+    int multiplication() { return a * b;}
+    // division
+    int division() { return a / b;}
+}
 
-        //creating a new user menu
-        System.out.println("Welcome to Calculator! Using OOP Methods");
-        System.out.println("This is the Main Menu");
-        System.out.println("Choose your exp<b></b>pression: ");
-        System.out.println("1. Addition /n 2.Substraction /n 3. Multiplication /n 4.Division");
+public class Main {
+    public static void main(String args[])
+    {
+        Scanner sr=new Scanner(System.in);
+        System.out.print("Enter the first Integer: ");
+        int a = sr.nextInt();
+        System.out.print("Enter the second Integer: ");
+        int b = sr.nextInt();
+        System.out.print("Enter the operation that you want to perform (+,-,*,/): ");
+        String op=sr.next();
+        int c = 0;
 
-        //creating a choice making for the user
-        int choice = input.nextInt();
+        Calculator obj = new Calculator(a, b);
+        switch(op)
+        {
+            case "+": c = obj.add();
+            break;
+            case "-": c = obj.subtract();
+            break;
+            case "*": c = obj.multiplication();
+            break;
+            case "/": c = obj.division();
+            break;
 
-        // creating switch cases for options
-        switch(choice){
-            case 1:
-                addition();
-
-            case 2:
-                substraction();
-
-            case 3:
-                multiplication();
-
-            case 4:
-                division();
+            default: System.out.println("Invalid!");
         }
-    }
 
-    //doing addition in this class
-    static void addition()
-    {
-        Scanner math1 = new Scanner(System.in);
-        double a;
-        double b;
-        double c;
+        System.out.println(a + op + b + "=" + c);
 
-        System.out.println("Enter the first number!");
-        a = math1.nextInt();
-        System.out.println("Second Number!");
-        b = math1.nextInt();
-        c = a + b;
-        System.out.println("The total value is" + c);
-        return;
-    }
-
-    static void substraction()
-    {
-        Scanner math1 = new Scanner(Systen.in);
-        double a;
-        double b;
-        double c;
-
-        System.out.println("Enter the first number!");
-        a = math1.nextInt();
-        System.out.println("Second Number!");
-        b = math1.nextInt();
-        c = a - b;
-        System.out.println("The total value is" + c);
-        return;
-
-    }
-
-    static void multiplication()
-    {
-        Scanner math1 = new Scanner(Systen.in);
-        double a;
-        double b;
-        double c;
-
-        System.out.println("First Number!");
-        a = math1.nextInt();
-        System.out.println("Second Number!");
-        b = math1.nextInt();
-        c = a * b;
-        System.out.println("The total value is" + c);
-        return;
-    }
-
-    static void division()
-    {
-        Scanner math1 = new Scanner(System.in);
-        double a;
-        double b;
-        double c;
-
-        System.out.println("First Number!");
-        a = math1.nextInt();
-        System.out.println("Second Number!");
-        b = math1.nextInt();
-        c = a / b;
-        System.out.println("The total value is" + c);
-        return;
     }
 }
